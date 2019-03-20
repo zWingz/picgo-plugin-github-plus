959190db75a6096bba45b4d4f98be5c13e61d6e4
zWingz <zhengzwing@gmail.com>
2019-03-20 17:38:22 +0800

    perf: ⚡️ rm zip key-value

--- lib/helper.ts
+++ lib/helper.ts
@@ -10,23 +10,16 @@ export function getNow () {
 export function zip (img: ImgType): ImgZipType {
   return {
     f: img.fileName,
-    w: img.width,
-    h: img.height,
-    id: img.id,
     s: img.sha
   }
 }
 
-export function unzip (
-  img: ImgZipType
-): ImgType {
-  const { f: fileName, w: width, h: height, id, s } = img
+export function unzip (img: ImgZipType): ImgType {
+  const { f: fileName, s } = img
   const extname = fileName.split('.').slice(-1)[0]
   return {
     fileName,
-    width,
-    height,
-    id,
+    id: s,
     sha: s,
     extname,
     imgUrl: '',
